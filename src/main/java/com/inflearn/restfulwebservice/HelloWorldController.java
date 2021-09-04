df90a5a32870366bc41fadb8f4e3bc38cc78c1b5
kanghangyul <gang6607@gmail.com>
2021-09-04 23:45:45 +0900

    2021 09 04

--- src/main/java/com/inflearn/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/inflearn/restfulwebservice/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.inflearn.restfulwebservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @RestController : @Controller + @ResponseBody
 @RestController
 public class HelloWorldController {
     // GET
@@ -16,7 +17,7 @@ public class HelloWorldController {
     // opt + enter : Class 자동 생성
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        // RestController Annotation을 선언해두었다면 response body에 따로 넣어 반환하지 않아도 자동으로 JSON 타입으로 반환한다.
+        // RestController Annotation을 선언해두었다면 ResponseBody에 포함하지 않아도 자동으로 JSON 타입으로 반환한다.
         return new HelloWorldBean("Hello World");
     }
 }
