8bfcf642a645841835d8d84d8603a8a4777a6aab
kanghangyul <gang6607@gmail.com>
2021-09-04 23:30:23 +0900

    2021 09 04

---
+++ src/main/java/com/inflearn/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,41 @@
+package com.inflearn.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data // lombok 'Data' Annotation : getter, setter, toString 등의 VO 구조(기능) 역할을 해 줌
+@AllArgsConstructor // lombok 'AllArgsConstructor' Annotation : 모든 변수들(arguments)에 해당하는 생성자를 만들어 줌
+@NoArgsConstructor // lombok 'NoArgsConstructor' Annotation : 기본 생성자를 만들어 줌
+public class HelloWorldBean {
+    private String message;
+
+    /**
+     * AllArgsConstructor Annotation으로 사자리게 되는 소스
+     */
+    /*
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+     */
+
+    /**
+     * Data Annotation으로 사라지게 되는 소스
+     */
+    /*
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String toString() {
+        return "message : " + this.message;
+    }
+
+    등
+    */
+}
--- src/main/java/com/inflearn/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/inflearn/restfulwebservice/HelloWorldController.java
@@ -12,4 +12,11 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+
+    // opt + enter : Class 자동 생성
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        // RestController Annotation을 선언해두었다면 response body에 따로 넣어 반환하지 않아도 자동으로 JSON 타입으로 반환한다.
+        return new HelloWorldBean("Hello World");
+    }
 }
