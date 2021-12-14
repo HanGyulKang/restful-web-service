df7d1e211f9b03ef837fa0b648f68f96718f0200
kanghangyul <gang6607@gmail.com>
2021-12-14 23:35:18 +0900

    2021 12 14

---
+++ src/main/java/com/inflearn/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,57 @@
+package com.inflearn.restfulwebservice.user;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+// GET    : 조회
+// POST   : 입력
+// PUT    : 수정
+// DELETE : 삭제
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    // 의존성 주입(생성자 이용)
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // Get /users/1 or /users/10 -> 1, 10번과 같은 경우 server로 전달할 때 String으로 전달
+    @GetMapping(path = "/users/{id}")
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        // 조회된 결과가 없을 경우(null 값을 return 받았을 경우 예외처리...
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // User Class의 JsonFilter를 활용한 방법 - S
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "password", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+        // User Class의 JsonFilter를 활용한 방법 - E
+
+        return mapping;
+    }
+}
--- src/main/java/com/inflearn/restfulwebservice/user/User.java
+++ src/main/java/com/inflearn/restfulwebservice/user/User.java
@@ -1,7 +1,8 @@
 package com.inflearn.restfulwebservice.user;
 
 //import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,7 +13,8 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 // value에 넣은 변수는 제외하고 사용자에게 return
-@JsonIgnoreProperties(value={"password", "ssn"})
+//@JsonIgnoreProperties(value={"password", "ssn"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
