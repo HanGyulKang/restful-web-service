572914e36b8834183b22fa57676f3aed3bc78dc8
kanghangyul <gang6607@gmail.com>
2021-12-14 23:23:07 +0900

    2021 12 14

--- src/main/java/com/inflearn/restfulwebservice/user/User.java
+++ src/main/java/com/inflearn/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.inflearn.restfulwebservice.user;
 
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,10 +11,17 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+// value에 넣은 변수는 제외하고 사용자에게 return
+@JsonIgnoreProperties(value={"password", "ssn"})
 public class User {
     private Integer id;
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
     @Past
     private Date joinDate;
+
+//    @JsonIgnore
+    private String password;
+//    @JsonIgnore
+    private String ssn;
 }
