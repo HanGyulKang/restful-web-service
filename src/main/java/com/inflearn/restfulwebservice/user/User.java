74553177f4e1c5f1c855ba9446c98d827de793db
kanghangyul <gang6607@gmail.com>
2021-12-20 22:00:59 +0900

    2021 12 20

--- src/main/java/com/inflearn/restfulwebservice/user/User.java
+++ src/main/java/com/inflearn/restfulwebservice/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 //import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -12,6 +13,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 // value에 넣은 변수는 제외하고 사용자에게 return
 //@JsonIgnoreProperties(value={"password", "ssn"})
 @JsonFilter("UserInfo")
