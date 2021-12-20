4cd4af07917be0d50e5dd00cbe5e532a0d74cb3c
kanghangyul <gang6607@gmail.com>
2021-12-20 22:32:50 +0900

    2021 12 20

--- src/main/java/com/inflearn/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/inflearn/restfulwebservice/user/AdminUserController.java
@@ -45,8 +45,11 @@ public class AdminUserController {
     }
 
     // Get /users/1 or /users/10 -> 1, 10번과 같은 경우 server로 전달할 때 String으로 전달
-    @GetMapping(path = "/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+//    @GetMapping(path = "/v1/users/{id}") // 1. url을 이용한 버전 관리
+//    @GetMapping(value = "/users/{id}", params = "version=1") // 2. parameter를 이용한 버전 관리
+//    @GetMapping(value = "/users/{id}", headers = "API-VERSION=1") // 3. header를 이용한 버전 관리
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.apiV1+xml") // 4. MIME을 이용한 버전 관리
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
 
         // 조회된 결과가 없을 경우(null 값을 return 받았을 경우 예외처리...
@@ -61,6 +64,35 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+        // User Class의 JsonFilter를 활용한 방법 - E
+
+        return mapping;
+    }
+
+//    @GetMapping(path = "/v2/users/{id}")
+//    @GetMapping(value = "/users/{id}", params = "version=2")
+//    @GetMapping(value = "/users/{id}", headers = "API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.apiV2+json")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        // 조회된 결과가 없을 경우(null 값을 return 받았을 경우 예외처리...
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        // User Class의 JsonFilter를 활용한 방법 - S
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
         // User Class의 JsonFilter를 활용한 방법 - E
 
