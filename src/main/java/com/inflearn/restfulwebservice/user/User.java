2c748c3074d61138408fcf2f09b8527cea151dd0
kanghangyul <gang6607@gmail.com>
2022-02-28 20:01:38 +0900

    2021 02 28

--- src/main/java/com/inflearn/restfulwebservice/user/User.java
+++ src/main/java/com/inflearn/restfulwebservice/user/User.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -20,11 +23,16 @@ import java.util.Date;
 //@JsonIgnoreProperties(value={"password", "ssn"})
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity // JPA : 해당하는 class명으로 테이블을 생성하고 정보로 column을 생성
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
+
     @Past
     @ApiModelProperty(notes = "등록일을 입력해 주세요.")
     private Date joinDate;
